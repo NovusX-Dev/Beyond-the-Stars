1a567a9f8f4869a8d970509ee220c09cd088e810
Novus X <cc.br@outlook.com>
2021-03-25 06:37:24 -0300

    Added simpe controls to main menu and quit functionality

--- Assets/Scripts/Main Menu/MainMenu.cs
+++ Assets/Scripts/Main Menu/MainMenu.cs
@@ -8,6 +8,14 @@ public class MainMenu : MonoBehaviour
 {
     [SerializeField] int sceneToLoad = 1;
 
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene(sceneToLoad);
