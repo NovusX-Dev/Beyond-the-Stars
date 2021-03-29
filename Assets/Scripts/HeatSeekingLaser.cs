12d618327ed75cf12b1f8c69615bd122e06196e2
Novus X <cc.br@outlook.com>
2021-03-29 14:06:07 -0300

    Code Programming: Homing Projectile

--- Assets/Scripts/HeatSeekingLaser.cs
+++ Assets/Scripts/HeatSeekingLaser.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class HeatSeekingLaser : MonoBehaviour
 {
+    //Code Programming also: Homing Projectile
+
     [SerializeField] float speed = 8f;
     [SerializeField] float rotationSpeed = 3f;
 
